1da319d5adea3b570c1141852d748083bac8105c
solomko2 <solomko2006@gmail.com>
2018-03-22 10:11:48 +0200

    fixed action getNews

--- src/modules/news/+state/actions/news.actions.js
+++ src/modules/news/+state/actions/news.actions.js
@@ -1,5 +1,9 @@
 import * as fromActionTypes from '../action_types/news.action_types';
 
+export const getNews = data => ({
+    type: fromActionTypes.GET_NEWS
+});
+
 export const getNewsSuccess = data => ({
     type: fromActionTypes.GET_NEWS_SUCCESS,
     payload: { data }
--- src/modules/news/+state/api/news.js
+++ src/modules/news/+state/api/news.js
@@ -4,7 +4,7 @@ import axios from "axios/index";
 export const getNews = () => async dispatch => {
     try {
         const response = await axios.get(process.env.REACT_APP_API_HOST);
-
+        dispatch(newsActions.getNews());
         await dispatch(newsActions.getNewsSuccess(response.data));
     } catch (err) {
         await dispatch(newsActions.getNewsFailure(err));
